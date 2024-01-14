d0e937534e420bf7b2f091dab39dc111572f2a22
Georgi N. Boshnakov <georgi.boshnakov@manchester.ac.uk>
2024-01-14 15:50:05 +0000

    version 3.5.8

--- R/Countr-package.R
+++ R/Countr-package.R
@@ -34,13 +34,14 @@
 #' @importFrom car Boot
 #' @importFrom lmtest lrtest
 #' @importFrom xtable xtable
+#'
+#' @importFrom stats family
 #' 
 #' @details
 #'
-#' The methodology is described in the forthcoming paper
-#' \insertCiteOnly{CountrJssArticle}{Countr}
-#' in the Journal of Statistical Software (included in the package as vignette
-#' \code{vignette('Countr_guide_paper', package = "Countr")}).
+#' The methodology is described by
+#' \insertCiteOnly{CountrJssArticle;textual}{Countr}.  The paper is included in
+#' the package as vignette \code{vignette('Countr_guide_paper', package = "Countr")}).
 #'
 #' The main function is \code{\link{renewalCount}}, see its documentation for
 #' examples.
@@ -51,16 +52,14 @@
 #'
 #' @references
 #'
-#' \insertRef{CountrJssArticle}{Countr}
-#' 
 #' \insertRef{baker2017event}{Countr}
 #'
 #' \insertRef{boshnakov2017bivariate}{Countr}
 #'
 #' \insertRef{cameron2013regression}{Countr}
 #'
-#' \insertRef{TarakEtAl2018jss}{Countr}
-#'
+#' \insertRef{CountrJssArticle}{Countr}
+#' 
 #' \insertRef{mcshane2008count}{Countr}
 #'
 #' \insertRef{winkelmann1995duration}{Countr}
@@ -79,6 +78,6 @@ utils::globalVariables(
 ## Rcheck doesn't complain about undefined variable 'groups' since there is a
 ## function groups() in 'dplyr'. However, at first sight at least, it seems that
 ## in 'group = groups' the right-hand side is the name of the column in the data
-## frame as symbol. When checking not that the above call invokes the formula
+## frame as symbol. When checking, note that the above call invokes the formula
 ## method of lattice::barchart and see also the panel function
 ## lattice::panel.barchart()
---
+++ inst/auto/REFERENCES.el
@@ -0,0 +1,18 @@
+(TeX-add-style-hook
+ "REFERENCES"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "CountrJssArticle"
+    "winkelmann1995duration"
+    "mcshane2008count"
+    "baker2017event"
+    "cameron2013regression"
+    "boshnakov2017bivariate"
+    "kharrat2017vigperf"
+    "kharrat2017vigvarcovar"
+    "kharrat2017vigfertility"
+    "kharrat2017vigfootball"
+    "kharrat2017vigquine"
+    "kharrat2017vigguide"))
+ '(or :bibtex :latex))
+
